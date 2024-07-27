4a3594c55d75940854cc80eb6ffe9ef97de5358a
me-js-bro <me.js.bro@gmail.com>
2024-07-27 23:13:45 +0600

    Designed the home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home sewwt home</h1>
+        <div className='home'>
+            <h1 className='header'>Let's create a strong password for you</h1>
+
+            <div className='extra'>
+                <div className='line'></div>
+                <h3>Write your favourite text here.</h3>
+                <div className='line'></div>
+            </div>
+
+            <input className="prompt" type="text" placeholder='Your Text'/>
+
+            <button className='submit'>
+                <div className='btn-line line1'>
+                </div>
+                generate
+                <div className='btn-line line2'>
+                </div>
+            </button>
         </div>
     );
 };
